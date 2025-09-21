71de06a3104e9a157eaf722d0c5e8aebd88dca42
Rayane300402 <raycaratboulsi@gmail.com>
2025-09-21 14:27:31 +0300

    task 3 return emotions

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,23 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    anger   = emotions.get("anger", 0)
+    disgust = emotions.get("disgust", 0)
+    fear    = emotions.get("fear", 0)
+    joy     = emotions.get("joy", 0)
+    sadness = emotions.get("sadness", 0)
+
+    scores = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness
+    }
+
+    scores["dominant_emotion"] = max(scores, key=scores.get)
+    return scores
